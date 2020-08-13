acc8f776eb997bb15df4d56c1a949d2e45f32963
Ubuntu <ubuntu@ip-172-31-34-135.ap-northeast-1.compute.internal>
2020-08-13 10:57:19 +0000

    my first commit

---
+++ hangman.py
@@ -0,0 +1,46 @@
+
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "________      ",
+              "|             ",
+              "|       |     ",
+              "|       0     ",
+              "|      /|\    ",
+              "|      / \    ",
+              "|             ",
+              ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("welcome to hangman!")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "please think one alphabet:"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print(" ".join(board))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("you win!")
+            print(" ".join(board))
+            win = True
+            break
+
+    if not win:
+        print("\n".join(stages[0:wrong+1]))
+        print("you lose! the answer is {}.".format(word))
+
+# hangman("cat")
+
+import random
+candidates = ["cat", "dog", "apple", "tree"]
+answer = random.choice(candidates)
+hangman(answer)
